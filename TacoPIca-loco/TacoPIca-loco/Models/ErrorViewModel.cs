b51033ed0778a209c5e3c3f5b64a4618c10924ef
matheustorsani <zkingdragoon@gmail.com>
2025-05-22 17:32:45 -0300

    fazendo pagina inicial

---
+++ TacoPIca-loco/TacoPIca-loco/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace TacoPIca_loco.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
