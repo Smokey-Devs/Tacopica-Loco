345306f12525708ad7189649136709eaacb0e7fd
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 <zkingdragoon@gmail.com>
2025-05-27 17:25:44 -0300

    Home page feito!

--- TacoPIca-loco/TacoPIca-loco/Controllers/LoginController.cs
+++ TacoPIca-loco/TacoPIca-loco/Controllers/LoginController.cs
@@ -13,7 +13,7 @@ public class HomeController : Controller
         _logger = logger;
     }
 
-    public IActionResult Login()
+    public IActionResult Index()
     {
         return View();
     }
